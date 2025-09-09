6f3831839b24e5df2225b74d152d0a691b142c57
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-09-09 18:01:11 -0500

    updated

--- index.js
+++ index.js
@@ -132,8 +132,6 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
       console.log('Service Worker registered with scope:', registration.scope);
-    }).catch(function (error) {
-      console.error('Service Worker registration failed:', error);
-    });
+    })
   });
 }
