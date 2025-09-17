436d968b93d6e6b6201bd33352b96d3c9bb896ab
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-09-16 19:37:08 -0500

    updated

--- index.js
+++ index.js
@@ -112,4 +112,7 @@ document.getElementById("mobileID-generator").addEventListener("submit", async f
 document.getElementById("darkModeToggle").addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
 });
-
+// Light mode toggle
+document.getElementById("lightModeToggle").addEventListener("click", function () {
+  document.body.classList.remove("dark-mode");
+});
